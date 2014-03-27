a630490b919855ce6525ee8c17a9b304c7d1fc42
Asfanur <asfanur.arafin@gmail.com>
2014-03-27 17:22:35 +1100

    Initial Commit

---
+++ Raining/RAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RAppDelegate.h
+//  Raining
+//
+//  Created by Asfanur Arafin on 27/03/2014.
+//  Copyright (c) 2014 asfanur arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Raining/RViewController.m
@@ -0,0 +1,29 @@
+//
+//  RViewController.m
+//  Raining
+//
+//  Created by Asfanur Arafin on 27/03/2014.
+//  Copyright (c) 2014 asfanur arafin. All rights reserved.
+//
+
+#import "RViewController.h"
+
+@interface RViewController ()
+
+@end
+
+@implementation RViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
